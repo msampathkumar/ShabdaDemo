9212126ca7e4320ba184d1cff370d946101252cc
Sampath Kumar <sam1990kumar@gmail.com>
2019-03-26 19:35:47 +0530

    Add separate Demo Page to see the recorded details

--- run.py
+++ run.py
@@ -4,20 +4,23 @@
 Migrate pyecharts and Flask with custom templates functions.
 """
 from __future__ import unicode_literals
-
-import backend
-# import httplib
-
 from flask import Flask, render_template, request, json, jsonify
 
-
+import backend
 
 app = Flask(__name__, static_folder='static', static_url_path='/static')
 
 
-@app.route("/")
+@app.route("/123")
 def index():
     return render_template('index.html')
+
+
+
+@app.route("/demo")
+@app.route("/")
+def demo_index():
+    return render_template('demo.html')
 
 
 @app.route("/upload", methods=['POST', 'GET'])
@@ -30,7 +33,7 @@ def _rest_audio_upload():
 
 @app.route("/rest/refresh_uploaded")
 def _rest_refresh_uploaded():
-    return jsonify(['static/data/temp/sample1.wav'])
+    return jsonify(['static/data/temp/sample1.wav', 'static/data/temp/sample2.wav'])
 
 
 @app.route("/rest/refresh_download")
@@ -39,5 +42,4 @@ def _rest_refresh_download():
 
 
 if __name__ == '__main__':
-
     app.run(debug=True)
